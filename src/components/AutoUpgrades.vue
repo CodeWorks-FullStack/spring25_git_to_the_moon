17960262d7d25b135e032eaa7a7d796be8d8a165
Jeremy Fowler <jembyfowler@gmail.com>
2025-04-01 12:49:48 -0600

    added interval

--- src/components/AutoUpgrades.vue
+++ src/components/AutoUpgrades.vue
@@ -1,9 +1,14 @@
 <script setup>
 import { AppState } from '@/AppState.js';
 import { Pop } from '@/utils/Pop.js';
-import { computed } from 'vue';
+import { computed, onMounted } from 'vue';
 
 const upgrades = computed(() => AppState.autoUpgrades)
+const autoPower = computed(() => upgrades.value.reduce((acc, upgrade) => acc + upgrade.quantity * upgrade.power, 0))
+
+onMounted(() => {
+  setInterval(() => AppState.cheese += autoPower.value, 3000)
+})
 
 function purchaseUpgrade(upgrade) {
   if (upgrade.price > AppState.cheese) return Pop.toast("Not enough money, honey")
@@ -17,7 +22,7 @@ function purchaseUpgrade(upgrade) {
 
 <template>
   <div class="upgrades">
-    <p>Total Auto Power</p>
+    <p>Total Auto Power {{ autoPower }} / 3s</p>
     <div class="upgrade" v-for="upgrade in upgrades" :key="upgrade.name">
       <p>{{ upgrade.name }}</p>
       <p>{{ upgrade.power }} * {{ upgrade.quantity }} -> {{ upgrade.power * upgrade.quantity }}</p>
