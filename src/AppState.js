ee0e6f3384fc563a9782b922f6834764b6ccc86f
Jake Overall <joverall22@gmail.com>
2025-04-01 12:46:20 -0600

    adds click upgraded data, iterates click upgrades

--- src/AppState.js
+++ src/AppState.js
@@ -1,4 +1,5 @@
 import { reactive } from 'vue'
+import ClickUpgrades from './components/ClickUpgrades.vue'
 
 // NOTE AppState is a reactive object to contain app level data
 export const AppState = reactive({
@@ -6,6 +7,10 @@ export const AppState = reactive({
   identity: null,
   /** @type {import('./models/Account.js').Account} user info from the database*/
   account: null,
-  cheese: 0
+  cheese: 0,
+  ClickUpgrades: [
+    { name: '🔪', power: 1, price: 5, quantity: 0 },
+    { name: '🪛', power: 5, price: 25, quantity: 0 }
+  ]
 })
 
--- src/components/ClickUpgrades.vue
+++ src/components/ClickUpgrades.vue
@@ -2,16 +2,24 @@
 import { AppState } from '../AppState';
 import { computed, ref, onMounted } from 'vue';
 
+const upgrades = computed(() => AppState.ClickUpgrades)
+
+function buyUpgrade(upgrade) {
+
+}
+
+
+
 </script>
 
 
 <template>
   <div class="upgrades">
     <p>Total Click Power</p>
-    <div class="upgrade">
-      <p>upgrade name</p>
-      <p>upgrade stats</p>
-      <button>purchase</button>
+    <div class="upgrade" v-for="u in upgrades" :key="u.name">
+      <p>{{ u.name }}</p>
+      <p>{{ u.quantity }} -> {{ u.power }} -> {{ u.quantity * u.power }}</p>
+      <button @click="buyUpgrade(u)">purchase {{ u.price }}</button>
     </div>
   </div>
 </template>
